44a054089bc31ec39a7deadb306b5fa509557d83
Fcc <abdelpre18@gmail.com>
2025-06-14 19:13:51 -0400

    cambio en el main

--- src/main.py
+++ src/main.py
@@ -17,7 +17,10 @@ def get_proveedor(id: str = Query(max_length=4,min_length=4)) ->  dict:
     if get_proveedor_service(id):
         return JSONResponse(content=get_proveedor_service(id), status_code=200)
     return JSONResponse(content={}, status_code=404)
-
-
-
+@app.get("/proveedores/producto/by_idpv", tags=["Proveedor"]) # retorna todos lo productos de un proveedor segun su ipPROVEEDOR
+def get_productos_proveedor(id: str = Query(..., max_length=4, min_length=4)) -> JSONResponse:
+    productos = get_producto_proveedor_service(id)
+    if productos:
+        return JSONResponse(content=productos, status_code=200)
+    return JSONResponse(content={"message": "No se encontraron productos para ese proveedor."}, status_code=404)
 
