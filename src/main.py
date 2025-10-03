6c1cb9520f8078b2b98bef474e43fd01097f1fab
Fcc <abdelpre18@gmail.com>
2025-10-02 22:19:53 -0400

    franz:hotfix/se quito el socket

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI ,WebSocket
+from fastapi import FastAPI 
 from src.model.usuario_modelo import Usuario
 
 from src.services.usuario_servicio import crear_usuario,obtener_usuarios,lista_usuarios
@@ -8,24 +8,15 @@ app = FastAPI(
     description="Una API básica para registrar y listar usuarios.",
     version="1.0.0"
 ) 
-usuarioActual=0
-@app.websocket("/ws")
-async def ws_endpoint(ws: WebSocket):
-    await ws.accept()
-    while True:
-        if usuarioActual!=len(lista_usuarios):
-            usuarioActual=len(lista_usuarios) 
-            
-            await ws.send_text(lista_usuarios[-1]) 
-            await asyncio.sleep(2) 
-            
+
 
 
 @app.post("/usuarios", response_model=Usuario)
-def crear_usuario(usuario: Usuario):
+def crear_usuario_nuevo(usuario: Usuario):
+    print(usuario)
     return crear_usuario(usuario)
 
-@app.get("/usuarios", response_model=list[Usuario])
+@app.get("/usuarios_get", response_model=list[Usuario])
 def listar_usuarios():
     return obtener_usuarios()
 if __name__ == "__main__":
