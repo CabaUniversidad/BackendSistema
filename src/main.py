8f103b7029628fafa1ccf9dcd1d6b83e702171a7
Fcc <abdelpre18@gmail.com>
2025-10-04 12:48:57 -0400

    franz:hotfix/cambio en el host

--- src/main.py
+++ src/main.py
@@ -1,24 +1,31 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI
 from src.model.usuario_modelo import Usuario
+from src.services.usuario_servicio import crear_usuario, obtener_usuarios
 
-from src.services.usuario_servicio import crear_usuario,obtener_usuarios,lista_usuarios
-import asyncio
 app = FastAPI(
     title="API de Usuarios",
     description="Una API básica para registrar y listar usuarios.",
     version="1.0.0"
-) 
+)
 
-
-
+# Endpoints
 @app.post("/usuarios", response_model=Usuario)
 def crear_usuario_nuevo(usuario: Usuario):
-    print(usuario)
     return crear_usuario(usuario)
 
 @app.get("/usuarios_get", response_model=list[Usuario])
 def listar_usuarios():
     return obtener_usuarios()
+
+
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("src.main:app", host="127.0.0.1", port=8000, reload=True)
+    import os
+
+    # Escuchar en 0.0.0.0 para que sea accesible desde otros contenedores
+    uvicorn.run(
+        "src.main:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=os.getenv("DEV_MODE") == "1"
+    )
