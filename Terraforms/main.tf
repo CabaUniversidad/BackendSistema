d01029b05c14a2f4089f33aa348793ede66beae5
Fcc <abdelpre18@gmail.com>
2025-10-28 12:30:51 -0400

    fix: Corregido el archivo deploy8

--- Terraforms/main.tf
+++ Terraforms/main.tf
@@ -21,7 +21,7 @@ provider "oci" {
   fingerprint  = var.fingerprint
   region       = var.region
 
-  # ¡Clave! Usa la ruta del archivo CONVERTIDO.
+  # ¡Clave! Usa la ruta del archivo temporal.
   private_key_path = var.private_key_path
 }
 
