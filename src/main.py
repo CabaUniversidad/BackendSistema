f518a7c937228774ec03f588abb93f5e224f5fb4
Fcc <abdelpre18@gmail.com>
2025-12-06 11:25:52 -0400

    franz:hotfix/se instalo las dependencias

--- src/main.py
+++ src/main.py
@@ -111,3 +111,8 @@ def editar_empleado(cod_empleado: str, request: EmpleadoEditarRequest):
     except Exception as e:
         print("ERROR al editar empleado:", str(e))
         raise HTTPException(status_code=400, detail=str(e))
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("src.main:app", host="127.0.0.1", port=8000, reload=True)
+#python -m src.main
